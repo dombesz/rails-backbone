99e8fcfbee1ba00975b9040c5b232175eaa32a3e
Dombi Attila <attila.dombi@evozon.com>
2012-10-25 11:20:54 +0300

    Initial commit

---
+++ app/assets/javascripts/backbone_rails.js.coffee
@@ -0,0 +1,9 @@
+window.BackboneRails =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  init: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  BackboneRails.init()
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneRails::Application.config.secret_token = '261754806d973061fe4580ba535ee9162978b0ba92d46bb6c3a504ec3f08947cffcabd01c5c28dcd5924d341b2ededbe801b0010d191d791966909723f4c1528'
